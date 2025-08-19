aa1cc360ee34c62cbedf6da2c1fef371540b557c
hathoriastro <ahmadtovichha@gmail.com>
2025-08-19 19:11:28 +0700

    menambahkan splash screen dan home sementara

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, '/homepage');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          "assets/images/logo_bukulapak.png",
+          width: 150,
+        ),
+      ),
+    );
+  }
+}
