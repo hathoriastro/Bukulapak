7aa4b7aaff4a043b1023e194a52756778c672836
hathoriastro <ahmadtovichha@gmail.com>
2025-08-21 22:14:16 +0700

    slicing bagian add_product_page.dart dan barterin_page.dart

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -8,3 +8,5 @@ const Color customGreen = Color(0xFF81C784); // Custom Green Color
 const Color lightBlue = Color(0xFF0476D0);
 const Color darkBlue = Color(0xFF154D71);
 const Color lightGray = Color(0xFF757575);
+const Color orange = Color(0xFFF29F05);
+const Color darkWhite = Color(0xFFEFEFEF);
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:bukulapak/pages/auth/welcomepage.dart';
 import 'package:bukulapak/pages/auth/sign_in_page.dart';
 import 'package:bukulapak/pages/auth/sign_up_page.dart';
 import 'package:bukulapak/pages/user/category_page.dart';
+import 'package:bukulapak/pages/user/add_product_page.dart';
+import 'package:bukulapak/pages/user/barterin_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,13 +21,15 @@ class MyApp extends StatelessWidget {
         fontFamily: 'poppins', // Set the global font family
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/category',
+      initialRoute: '/add_product',
       routes: {
         '/splash': (context) => const SplashScreen(),
         '/welcome': (context) => const WelcomePage(),
         '/sign_in': (context) => const SignInPage(),
         '/sign_up': (context) => const SignUpPage(),
         '/category': (context) => const CategoryPage(),
+        '/add_product': (context) => const AddProductPage(),
+        '/barterin': (context) => const BarterinPage(),
       },
     );
   }
