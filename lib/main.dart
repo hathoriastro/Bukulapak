e4269b7f2126c7f578588b020aee73249a5a6af3
naruuu62 <nurilarifin270@gmail.com>
2025-08-23 22:04:02 +0700

    fixing pubspec yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bukulapak/pages/splash_screen.dart';
+import 'package:bukulapak/pages/user/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bukulapak/pages/auth/welcomepage.dart';
@@ -22,13 +23,14 @@ class MyApp extends StatelessWidget {
         fontFamily: 'poppins', // Set the global font family
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/sign_up',
+      initialRoute: '/home',
       routes: {
         '/splash': (context) => const SplashScreen(),
         '/welcome': (context) => const WelcomePage(),
         '/sign_in': (context) => const SignInPage(),
         '/sign_up': (context) => const SignUpPage(),
         '/category': (context) => const CategoryPage(),
+        '/home' : (context) => const HomePage()
       },
     );
   }
